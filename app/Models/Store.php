94fff602a47fc41e3788559e3aad0eae7ab2fd62
uenomanami <xi2.ut0626@gmail.com>
2023-01-19 00:43:57 +0900

    stores・areas・categoriesテーブル作成、データ挿入

---
+++ app/Models/Store.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'category_id', 'area_id', 'description', 'image_url'];
+
+}
---
+++ database/migrations/2023_01_18_152742_create_stores_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedInteger('category_id');
+            $table->unsignedInteger('area_id');
+            $table->text('description');
+            $table->string('image_url');
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
