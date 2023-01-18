b43a0f01e6aefa967df2fac8b68fe8b91e273779
uenomanami <xi2.ut0626@gmail.com>
2023-01-18 11:57:16 +0900

    ログイン機能

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,40 @@
 <x-guest-layout>
     <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
+        <x-slot name="logo" class="logo">
+            @include('parts.header')
         </x-slot>
+
 
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
         <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        <x-auth-validation-errors class="mb-4 validation__error-red" :errors="$errors" />
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+        <div class="auth-common__form">
+            <p class="auth-common__form-title">Login</p>
+            <div class="auth-common__form-item">
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                    <!-- Email Address -->
+                    <div class="auth-common__email-wrap">
+                        <input id="email" class="auth-common__address" type="email" name="email" placeholder="Email" value="{{ old('email') }}" required />
+                    </div>
+
+                <!-- Password -->
+                    <div class="auth-common__pass-wrap">
+                        <input id="password" class="auth-common__password" type="password" name="password" required autocomplete="new-password" placeholder="Password" />
+                    </div>
+
+                    <div class="auth-common__submit-wrap">
+                        <button class="auth-common__submit">ログイン</button>
+                    </div>
+                </form>
+
             </div>
+        </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
     </x-auth-card>
 </x-guest-layout>
