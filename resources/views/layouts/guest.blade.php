b9ecf6b1cb233b964a8de46eefca354acab31c5b
uenomanami <xi2.ut0626@gmail.com>
2023-01-18 14:14:03 +0900

    会員登録機能修正2

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,6 +11,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
         <link rel="stylesheet" href="{{ asset('css/header.css') }}">
         <link rel="stylesheet" href="{{ asset('css/auth/common.css') }}">
 
