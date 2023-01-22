998e9cce797663027a0ac45037d33132403fdf91
uenomanami <xi2.ut0626@gmail.com>
2023-01-23 00:02:20 +0900

    お気に入り追加機能・削除機能

--- database/migrations/2023_01_21_155952_create_stores_table.php
+++ database/migrations/2023_01_21_155952_create_stores_table.php
@@ -6,32 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateStoresTable extends Migration
 {
-  /**
-   * Run the migrations.
-   *
-   * @return void
-   */
-  public function up()
-  {
-    Schema::create('stores', function (Blueprint $table) {
-      $table->id();
-      $table->string('name');
-      $table->foreignId('category_id')->constrained();
-      $table->foreignId('area_id')->constrained();
-      $table->text('description');
-      $table->string('image_url');
-      $table->timestamp('created_at')->useCurrent()->nullable();
-      $table->timestamp('updated_at')->useCurrent()->nullable();
-    });
-  }
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('area_id')->constrained();
+            $table->text('description');
+            $table->string('image_url');
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+        });
+    }
 
-  /**
-   * Reverse the migrations.
-   *
-   * @return void
-   */
-  public function down()
-  {
-    Schema::dropIfExists('stores');
-  }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
 }
--- database/migrations/2023_01_21_160022_create_favorites_table.php
+++ database/migrations/2023_01_21_160022_create_favorites_table.php
@@ -6,29 +6,29 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateFavoritesTable extends Migration
 {
-  /**
-   * Run the migrations.
-   *
-   * @return void
-   */
-  public function up()
-  {
-    Schema::create('favorites', function (Blueprint $table) {
-      $table->id();
-      $table->foreignId('user_id')->constrained();
-      $table->foreignId('store_id')->constrained();
-      $table->timestamp('created_at')->useCurrent()->nullable();
-      $table->timestamp('updated_at')->useCurrent()->nullable();
-    });
-  }
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('store_id')->constrained();
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+        });
+    }
 
-  /**
-   * Reverse the migrations.
-   *
-   * @return void
-   */
-  public function down()
-  {
-    Schema::dropIfExists('favorites');
-  }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
 }
