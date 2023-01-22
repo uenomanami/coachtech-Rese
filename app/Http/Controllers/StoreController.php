52a2ff73c45e374379838c3375d716c0f3f1add8
uenomanami <xi2.ut0626@gmail.com>
2023-01-23 00:05:58 +0900

    お気に入り追加機能・削除機能

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -1,24 +1,31 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Store;
+use Illuminate\Support\Facades\Auth;
 
 
 use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
         $stores = Store::all();
-        return view('index', ['stores'=>$stores]);
+        $user = Auth::user();
+        $param = [
+            'stores' => $stores,
+            'user' => $user
+        ];
+        return view('index', $param);
     }
+
     public function detail(Request $request)
     {
         $store_id = $request->input('store_id');
-        
         $store = Store::doSearch($store_id);
 
-        return view('detail', compact("store"));
+        return view('detail', ['store' => $store]);
     }
 }
